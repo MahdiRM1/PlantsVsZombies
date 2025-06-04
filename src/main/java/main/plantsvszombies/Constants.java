54a643783862738425a9cbbf6d8f9bf4928a905a
Mahdi <mahdi@example.com>
2025-06-04 13:03:44 +0330

    initial commit

---
+++ src/main/java/main/plantsvszombies/Constants.java
@@ -0,0 +1,10 @@
+package main.plantsvszombies;
+
+import javafx.stage.Screen;
+
+public final class Constants {
+    public static final double width = Screen.getPrimary().getBounds().getWidth();
+    public static final double height = Screen.getPrimary().getBounds().getHeight();
+    public static final double TILE_HEIGHT = height/7;
+    public static final double TILE_WIDTH = width/10;
+}
