3a16267e3251db8b7262efdf47946b3bf74d7734
Sahar <saharGolestani@example.com>
2025-06-04 15:24:56 +0330

    first commit done

--- src/main/java/main/plantsvszombies/Constants.java
+++ src/main/java/main/plantsvszombies/Constants.java
@@ -7,4 +7,5 @@ public final class Constants {
     public static final double height = Screen.getPrimary().getBounds().getHeight();
     public static final double TILE_HEIGHT = height/7;
     public static final double TILE_WIDTH = width/10;
+
 }
---
+++ src/main/java/main/plantsvszombies/PeaPlant.java
@@ -0,0 +1,8 @@
+package main.plantsvszombies;
+
+public class PeaPlant extends Plant{
+    // Bullet
+//    public PeaPlant() {
+//        super();
+//    }
+}
