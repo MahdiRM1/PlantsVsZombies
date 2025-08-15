3e883bf9e4049588436605f5b543ba4ff782c065
Mahdi <mahdi@example.com>
2025-08-15 17:45:43 +0330

    fix a bug

--- src/main/java/main/plantsvszombies/Zombies/OriginalZombie.java
+++ src/main/java/main/plantsvszombies/Zombies/OriginalZombie.java
@@ -31,7 +31,7 @@ public class OriginalZombie extends Zombie {
     public OriginalZombie(int row, int col) {
         super(row, col);
         HP = 100;
-        speed = 5;
+        speed = 1;
     }
 
     public static Image[] getimages(ZombieState state) {
